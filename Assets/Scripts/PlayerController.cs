1594a1c19fbb4612d1e8cf88d46798727d9e9a84
재훈 정 <ghdwp98@naver.com>
2024-03-06 14:19:35 +0900

    docs : 플레이어 velocity 달리기

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,12 @@
-using System.Runtime.CompilerServices;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
     //각각의 상태 클래스에서 뭘 할지 결정하고 어떤 상황이 되면 다른 상태로 트랜지션 할지 결정 
     //이동은 그냥 고전시스템으로 처리하자 그게 훨씬 쉬울것 같다. 
+    // groundCheck가 필요한 친구들은 피봇을 바텀으로 두고 쓰자 
 
-    public enum State { Idle, Run, Attack, Jump ,attackRun,JumpAttack,anchor}
+    public enum State { Idle, Run, Attack, Jump, attackRun, JumpAttack, anchor }
     //앵커 c키 누르면 이동 없이  8방향 조준 전환 가능 
 
     [Header("Player")]
@@ -25,16 +23,20 @@ public class PlayerController : MonoBehaviour
     [Header("Spec")]
     [SerializeField] float maxSpeed = 5.0f;
     [SerializeField] float accelPower = 10.0f;
-    [SerializeField] float decelPower = 10.0f;
-    [SerializeField] float jumpSpeed = 9.0f;
+    [SerializeField] float decelPower = 20.0f;
+    [SerializeField] float jumpSpeed = 15.0f;
     [SerializeField] LayerMask groundCheckLayer; //땅위에서만 점프 가능 or 패리 위에서만 점프가능 
     [SerializeField] Vector2 playerPos;
 
     private Vector2 inputDir;
-    private bool isGround;
+    private bool onGround;
     private int groundCount;
+    
 
     private StateMachine stateMachine;
+
+    string nowAnime = "";
+    string oldAnime = "";
 
     //이동 앉기 대시 점프 조준 슈팅 -->상태머신 구현 
 
@@ -47,10 +49,11 @@ public class PlayerController : MonoBehaviour
         stateMachine.AddState(State.Jump, new JumpState(this));
         stateMachine.InitState(State.Idle); //최초 상태를 Idle 상태로 시작 
 
-
     }
     private void Start()
     {
+        nowAnime = "IdlePlayer";
+        oldAnime = "IdlePlayer";
         rigidbody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         SpriteRenderer renderer = GetComponent<SpriteRenderer>();
@@ -75,9 +78,9 @@ public class PlayerController : MonoBehaviour
     {
         protected PlayerController player; //Player로 이를 상속하는 state클래스들에서
         // player.hp 등으로 플레이어의 변수를 이용할 수 있도록 한다. 
-        protected Transform transform=>player.transform;
+        protected Transform transform => player.transform;
         protected int hp { get { return player.hp; } set { player.hp = value; } }
-        protected int mp {get { return player.mp; } set{ player.mp = value; } }
+        protected int mp { get { return player.mp; } set { player.mp = value; } }
         protected float axisH { get { return player.axisH; } set { player.axisH = value; } }
         protected float axisV { get { return player.axisV; } set { player.axisV = value; } }
         protected Rigidbody2D rigidbody => player.rigidbody;
@@ -87,15 +90,15 @@ public class PlayerController : MonoBehaviour
         protected float decelPower => player.decelPower;
         protected float accelPower => player.accelPower;
         protected float jumpSpeed => player.jumpSpeed;
-        protected LayerMask groundCheckLayer=>player.groundCheckLayer;
+        protected LayerMask groundCheckLayer => player.groundCheckLayer;
         protected Vector2 playerPos => player.playerPos;
-        protected bool isGround=>player.isGround;
-        private int groundCount => player.groundCount;
+        protected bool onGround { get { return player.onGround; } set { player.onGround = value; } }
+        protected int groundCount { get { return player.groundCount; } set { player.groundCount = value; } }
 
-       
+
         public PlayerState(PlayerController player)
         {
-            this.player=player;
+            this.player = player;
         }
 
     }
@@ -107,25 +110,59 @@ public class PlayerController : MonoBehaviour
 
         public override void Update() //계속 돌아가면서 체크 업데이트 + 트랜지션 
         {
-            
+            animator.Play("IdlePlayer");
             axisH = Input.GetAxisRaw("Horizontal");
             axisV = Input.GetAxisRaw("Vertical"); //점프가 아니라 위 아래 보는 느낌으로?
-            
-        }
 
+        }
         public override void Transition()
         {
-            if(axisH!=0) //이동이 있으면 상태전환 
+            if (axisH != 0) //이동이 있으면 상태전환 
             {
-                
+
                 ChangeState(State.Run);
             }
-            
+
+            if(Input.GetKeyDown(KeyCode.Z))
+            {
+                ChangeState(State.Jump);
+            }
+
+
         }
     }
     private class JumpState : PlayerState
     {
         public JumpState(PlayerController player) : base(player) { }
+
+        public override void Enter()
+        {
+            Debug.Log("a");
+            Jump();
+            animator.Play("JumpPlayer");
+        }
+
+        public override void FixedUpdate()
+        {
+            onGround = Physics2D.Linecast(transform.position, transform.position - (transform.up * 0.1f), groundCheckLayer);
+
+            
+        }
+        public override void Transition()
+        {
+            if(onGround)
+            {
+                ChangeState(State.Idle);
+            }
+        }
+
+        public void Jump()
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpSpeed);
+        }
+
+        
+
     }
 
     private class AttackState : PlayerState
@@ -144,43 +181,49 @@ public class PlayerController : MonoBehaviour
             axisH = Input.GetAxisRaw("Horizontal");
             axisV = Input.GetAxisRaw("Vertical"); //점프가 아니라 위 아래 보는 느낌으로?
 
-            if (axisH < 0.0f&&rigidbody.velocity.x>-maxSpeed) //왼쪽 이동
+            if (axisH < 0.0f && rigidbody.velocity.x > -maxSpeed) //왼쪽 이동
             {
-                renderer.flipX = true;  //오른쪽으로 모습 바꿔주기
-                rigidbody.velocity = new Vector2(axisH * accelPower, rigidbody.velocity.y);
 
+                rigidbody.velocity = new Vector2(axisH * accelPower, rigidbody.velocity.y);
+                animator.Play("RunPlayer");
+                renderer.flipX = true;  //왼쪽으로 모습 바꿔주기
 
             }
-            else if (axisH > 0.0f&&rigidbody.velocity.x<maxSpeed) //오른쪽 이동 항상 일정한 속도 
+            else if (axisH > 0.0f && rigidbody.velocity.x < maxSpeed) //오른쪽 이동 항상 일정한 속도 
             {
-                renderer.flipX = false;  //다시 왼쪽으로 (왼쪽이 defalut)
+
+
                 rigidbody.velocity = new Vector2(axisH * accelPower, rigidbody.velocity.y);
+                animator.Play("RunPlayer");
+                renderer.flipX = false;  //오른쪽으로 (오른쪽이 디폴트)
 
             }
-
             //감속상태 --> 일정속도 유지 및 정지시 바로 멈추도록 
-            if(axisH==0&&rigidbody.velocity.x>0.1f) //오른쪽으로 이동중인 상태에서 멈추면 
+            if (axisH == 0 && rigidbody.velocity.x > 0.1f) //오른쪽으로 이동중인 상태에서 멈추면 
             {
-
+                rigidbody.velocity = Vector3.zero;
             }
-            
-
-
-
+            else if (axisH == 0 && rigidbody.velocity.x < -0.1f) //왼쪽 이동 중 정지 
+            {
+                rigidbody.velocity = Vector3.zero;
+            }
         }
-
         public override void Transition() //트랜지션에서 달리면서 쏘기 달리면서 점프 등등 전환구현 
         {
             if (rigidbody.velocity.x == 0.0f) //속도가 0 일 때 (움직임이 없을 때 idle로 체인지 해주기 )
             {
-                ChangeState(State.Idle);  
+                ChangeState(State.Idle);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Z))
+            {
+                ChangeState(State.Jump);
             }
         }
 
     }
+    
 
-   
-    
-    
+
 
 }
