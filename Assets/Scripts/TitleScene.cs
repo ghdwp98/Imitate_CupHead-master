cf05bd772589d6f180059c4b7abdf7e8dcc35ca2
재훈 정 <ghdwp98@naver.com>
2024-03-19 18:02:13 +0900

    슬라임 체력 500으로 복구

--- Assets/Scripts/TitleScene.cs
+++ Assets/Scripts/TitleScene.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
-using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.Port;
 
 public class TitleScene : BaseScene
 {
